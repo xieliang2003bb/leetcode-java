e41d38e92d90b6a2ec52f4acc657a481d8485e50
zsxwing <zsxwing@gmail.com>
2013-09-20 22:51:04 +0800

    Solve the 'Climbing Stairs' question

--- src/main/java/climbing_stairs/ClimbingStairs.java
+++ src/main/java/climbing_stairs/ClimbingStairs.java
@@ -4,15 +4,9 @@ public class ClimbingStairs {
 
     public class Solution {
         public int climbStairs(int n) {
-            if (n == 1) {
-                return 1;
-            }
-            if (n == 2) {
-                return 2;
-            }
             int f1 = 1;
-            int f2 = 2;
-            for (int i = 3; i <= n; i++) {
+            int f2 = 1;
+            for (int i = 2; i <= n; i++) {
                 int temp = f1 + f2;
                 f1 = f2;
                 f2 = temp;
