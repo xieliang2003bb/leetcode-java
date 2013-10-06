eb0cc356c958cdab3a2231d303987d285ec6278b
zsxwing <zsxwing@gmail.com>
2013-10-06 21:20:58 +0800

    Updated the solution

--- src/main/java/search_insert_position/SearchInsertPosition.java
+++ src/main/java/search_insert_position/SearchInsertPosition.java
@@ -10,10 +10,8 @@ public class SearchInsertPosition {
                 int mid = left + (right - left) / 2;
                 if (A[mid] < target) {
                     left = mid + 1;
-                } else if (A[mid] > target) {
-                    right = mid - 1;
                 } else {
-                    return mid;
+                    right = mid - 1;
                 }
             }
             return left;
