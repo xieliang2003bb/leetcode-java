0e6c81f41254641fbd07d7ff3d74421ab869fc21
zsxwing <zsxwing@gmail.com>
2013-10-06 21:29:46 +0800

    Solve the 'Maximum Subarray' question

---
+++ src/main/java/maximum_subarray/MaximumSubarray.java
@@ -0,0 +1,20 @@
+package maximum_subarray;
+
+public class MaximumSubarray {
+
+    public class Solution {
+        public int maxSubArray(int[] A) {
+            int sum = A[0];
+            int maxSum = A[0];
+            for (int i = 1; i < A.length; i++) {
+                sum = Math.max(sum + A[i], A[i]);
+                maxSum = Math.max(maxSum, sum);
+            }
+            return maxSum;
+        }
+    }
+
+    public static class UnitTest {
+
+    }
+}
