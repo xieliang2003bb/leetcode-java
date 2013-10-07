05b41c9bd63e7c638f3a65b0075a41a15cd55414
zsxwing <zsxwing@gmail.com>
2013-10-07 22:39:32 +0800

    Update the solution

--- src/main/java/binary_tree_level_order_traversal/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/binary_tree_level_order_traversal/BinaryTreeLevelOrderTraversal.java
@@ -7,29 +7,22 @@ import common.TreeNode;
 public class BinaryTreeLevelOrderTraversal {
 
     public class Solution {
+        private void search(TreeNode root, int level,
+                ArrayList<ArrayList<Integer>> ans) {
+            if (root == null) {
+                return;
+            }
+            if (level >= ans.size()) {
+                ans.add(new ArrayList<Integer>());
+            }
+            ans.get(level).add(root.val);
+            search(root.left, level + 1, ans);
+            search(root.right, level + 1, ans);
+        }
+
         public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
             ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-            if (root == null) {
-                return ans;
-            }
-            ArrayList<TreeNode> queue = new ArrayList<TreeNode>();
-            int start = 0;
-            queue.add(root);
-            while (start != queue.size()) {
-                ArrayList<Integer> level = new ArrayList<Integer>();
-                int len = queue.size();
-                for (; start < len; start++) {
-                    TreeNode node = queue.get(start);
-                    if (node.left != null) {
-                        queue.add(node.left);
-                    }
-                    if (node.right != null) {
-                        queue.add(node.right);
-                    }
-                    level.add(node.val);
-                }
-                ans.add(level);
-            }
+            search(root, 0, ans);
             return ans;
         }
     }
