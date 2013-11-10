b2dbeecd2c388b26a3dc9fd0a3eb0ae33c46354a
zsxwing <zsxwing@gmail.com>
2013-11-10 23:27:12 +0800

    Update the solution

--- src/main/java/reverse_linked_list_ii/ReverseLinkedListII.java
+++ src/main/java/reverse_linked_list_ii/ReverseLinkedListII.java
@@ -6,34 +6,30 @@ public class ReverseLinkedListII {
 
     public class Solution {
         public ListNode reverseBetween(ListNode head, int m, int n) {
-            ListNode prefix = null;
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode prefix = dummy;
             int count = n - m + 1;
             m--;
             while (m > 0) {
-                prefix = prefix == null ? head : prefix.next;
+                prefix = prefix.next;
                 m--;
             }
             ListNode pre = null;
-            ListNode p = prefix == null ? head : prefix.next;
+            ListNode p = prefix.next;
             ListNode reversedTail = p;
             while (count > 0) {
                 ListNode temp = p.next;
-                if (pre != null) {
-                    p.next = pre;
-                }
+                p.next = pre;
                 pre = p;
                 p = temp;
                 count--;
             }
-            if (prefix == null) {
-                head = pre;
-            } else {
-                prefix.next = pre;
-            }
+            prefix.next = pre;
             if (reversedTail != null) {
                 reversedTail.next = p;
             }
-            return head;
+            return dummy.next;
         }
     }
 
