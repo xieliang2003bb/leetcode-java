b09d36179adc1d967a10d4c8931e3b92b4d7fd46
zsxwing <zsxwing@gmail.com>
2013-11-10 13:42:02 +0800

    Update the solution

--- src/main/java/binary_tree_preorder_traversal/BinaryTreePreorderTraversal.java
+++ src/main/java/binary_tree_preorder_traversal/BinaryTreePreorderTraversal.java
@@ -1,5 +1,6 @@
 package binary_tree_preorder_traversal;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 import common.TreeNode;
@@ -20,6 +21,25 @@ public class BinaryTreePreorderTraversal {
         public ArrayList<Integer> preorderTraversal(TreeNode root) {
             ArrayList<Integer> preorder = new ArrayList<Integer>();
             preorderTraversal(root, preorder);
+            return preorder;
+        }
+
+        public ArrayList<Integer> preorderTraversalWithIterative(TreeNode root) {
+            ArrayList<Integer> preorder = new ArrayList<Integer>();
+            ArrayDeque<TreeNode> stack = new ArrayDeque<TreeNode>();
+            if (root != null) {
+                stack.offerLast(root);
+                while (!stack.isEmpty()) {
+                    TreeNode p = stack.removeLast();
+                    while (p != null) {
+                        preorder.add(p.val);
+                        if (p.right != null) {
+                            stack.offerLast(p.right);
+                        }
+                        p = p.left;
+                    }
+                }
+            }
             return preorder;
         }
     }
