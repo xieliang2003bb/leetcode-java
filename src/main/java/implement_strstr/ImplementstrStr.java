000d0dafcb90ab66f31ac8f545479abb4ba4a758
zsxwing <zsxwing@gmail.com>
2015-01-04 23:18:04 +0800

    Use indexOf

--- src/main/java/implement_strstr/ImplementstrStr.java
+++ src/main/java/implement_strstr/ImplementstrStr.java
@@ -3,23 +3,8 @@ package implement_strstr;
 public class ImplementstrStr {
 
     public class Solution {
-        public String strStr(String haystack, String needle) {
-            if (haystack == null || needle == null) {
-                return null;
-            }
-            for (int i = 0; i <= haystack.length() - needle.length(); i++) {
-                boolean find = true;
-                for (int j = 0; j < needle.length(); j++) {
-                    if (haystack.charAt(i + j) != needle.charAt(j)) {
-                        find = false;
-                        break;
-                    }
-                }
-                if (find) {
-                    return haystack.substring(i);
-                }
-            }
-            return null;
+        public int strStr(String haystack, String needle) {
+            return haystack.indexOf(needle);
         }
     }
 
