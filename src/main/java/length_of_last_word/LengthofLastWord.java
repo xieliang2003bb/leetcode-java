a24bd81a1af72930fe0d4e8470e59959ec0d263e
zsxwing <zsxwing@gmail.com>
2015-02-05 22:51:10 +0800

    Scan in the reverse direction

--- src/main/java/length_of_last_word/LengthofLastWord.java
+++ src/main/java/length_of_last_word/LengthofLastWord.java
@@ -4,23 +4,17 @@ public class LengthofLastWord {
 
     public class Solution {
         public int lengthOfLastWord(String s) {
+            int i = s.length() - 1;
+            while (i >= 0 && s.charAt(i) == ' ') {
+                i--;
+            }
+            if (i < 0) {
+                return 0;
+            }
             int len = 0;
-            int i = 0;
-            while (true) {
-                while (i < s.length() && s.charAt(i) == ' ') {
-                    i++;
-                }
-                if (i == s.length()) {
-                    break;
-                }
-                int start = i;
-                while (i < s.length() && s.charAt(i) != ' ') {
-                    i++;
-                }
-                len = i - start;
-                if (i == s.length()) {
-                    break;
-                }
+            while (i >= 0 && s.charAt(i) != ' ') {
+                len++;
+                i--;
             }
             return len;
         }
