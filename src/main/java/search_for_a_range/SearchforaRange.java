0d8ced053aee15f74a8392b880160f2fcc6b0378
Leon Tse <leontse@hotmail.com>
2017-10-16 10:34:32 -0700

    add search_for_a_range

--- src/main/java/search_for_a_range/SearchforaRange.java
+++ src/main/java/search_for_a_range/SearchforaRange.java
@@ -4,29 +4,25 @@ public class SearchforaRange {
 
     public class Solution {
 
-        private int lowerBound(int[] A, int left, int right, int target) {
-            while (left <= right) {
+        public int[] searchRange(int[] nums, int target) {
+            int[] res = new int[] {-1, -1};
+            if (nums.length == 0) return res;
+            int left = 0, right = nums.length - 1;
+            while (left < right) {
                 int mid = left + (right - left) / 2;
-                if (A[mid] < target) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+                if (nums[mid] < target) left = mid + 1;
+                else right = mid;
             }
-            return left;
-        }
-
-        public int[] searchRange(int[] A, int target) {
-            int[] ans = new int[2];
-            int begin = lowerBound(A, 0, A.length - 1, target);
-            if (begin < A.length && A[begin] == target) {
-                ans[0] = begin;
-                ans[1] = lowerBound(A, begin + 1, A.length - 1, target + 1) - 1;
-            } else {
-                ans[0] = -1;
-                ans[1] = -1;
+            if (nums[right] != target) return res;
+            res[0] = right;
+            right = nums.length; // edge case - left=0, right=0
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] <= target) left = mid + 1;
+                else right= mid;
             }
-            return ans;
+            res[1] = left - 1;
+            return res;
         }
     }
 
