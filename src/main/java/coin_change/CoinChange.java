5c0738a074f75d6110690a55fc3b3f781b91111c
Leon Tse <leontse@hotmail.com>
2017-11-15 02:38:47 -0800

    add coin_change

---
+++ src/main/java/coin_change/CoinChange.java
@@ -0,0 +1,27 @@
+package coin_change;
+
+/**
+ * Created by lxie on 11/15/17.
+ */
+public class CoinChange {
+    public class Solution {
+        public int coinChange(int[] coins, int amount) {
+            int[] dp = new int[amount+1];
+            for(int i=0; i< dp.length; ++i) dp[i] = amount+1;
+            dp[0] = 0;
+            for (int i = 1; i <= amount; ++i) {
+                for (int j = 0; j < coins.length; ++j) {
+                    if (coins[j] <= i) {
+                        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                    }
+                }
+            }
+            return dp[amount] > amount ? -1 : dp[amount];
+        }
+
+    }
+
+    public static class UnitTest {
+
+    }
+}
