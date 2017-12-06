da3c2f65341e91c94c6b0c567228945dfd582343
Leon Tse <leontse@hotmail.com>
2017-12-06 01:41:20 -0800

    add lca_binary_tree

---
+++ src/main/java/lowest_common_ancestor_BTree/LCABinaryTree.java
@@ -0,0 +1,26 @@
+package lowest_common_ancestor_BTree;
+
+import common.TreeNode;
+
+/**
+ * Created by lxie on 12/6/17.
+ */
+public class LCABinaryTree {
+
+    public class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null || p == root || q == root) return root;
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p , q);
+            if (left != null && right != null) return root;
+            return left != null ? left : right;
+        }
+
+    }
+
+    public static class UnitTest {
+
+    }
+
+
+}
