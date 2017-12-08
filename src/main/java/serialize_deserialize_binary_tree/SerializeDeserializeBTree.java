be866b2c70dcf256d7f6fb81d20eacaafe2a8bf9
Leon Tse <leontse@hotmail.com>
2017-12-08 11:44:47 -0800

    modify serialize_deserialize_binary_tree

--- src/main/java/serialize_deserialize_binary_tree/SerializeDeserializeBTree.java
+++ src/main/java/serialize_deserialize_binary_tree/SerializeDeserializeBTree.java
@@ -2,9 +2,7 @@ package serialize_deserialize_binary_tree;
 
 import common.TreeNode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedList;
 
 /**
  * Created by lxie on 12/7/17.
@@ -14,41 +12,72 @@ public class SerializeDeserializeBTree {
     public static class Solution {
         public static class Codec {
 
-            // Encodes a tree to a single string.
             public static String serialize(TreeNode root) {
-                String[] out = {""};
-                serialize(root, out);
-                return out[0];
-            }
-
-            private static void serialize(TreeNode root, String[] out) {
-                if (root != null) {
-                    out[0]  +=  Integer.toString(root.val) + ' ';
-                    serialize(root.left, out);
-                    serialize(root.right, out);
-                } else {
-                    out[0] += "# ";
+                if(root==null){
+                    return "";
                 }
-            }
 
+                StringBuilder sb = new StringBuilder();
+
+                LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+
+                queue.add(root);
+                while(!queue.isEmpty()){
+                    TreeNode t = queue.poll();
+                    if(t!=null){
+                        sb.append(String.valueOf(t.val) + ",");
+                        queue.add(t.left);
+                        queue.add(t.right);
+                    }else{
+                        sb.append("#,");
+                    }
+                }
+
+                sb.deleteCharAt(sb.length()-1);
+                System.out.println(sb.toString());
+                return sb.toString();
+            }
 
             // Decodes your encoded data to tree.
             public static TreeNode deserialize(String data) {
-                if (data == null) return null;
-                String[] input = data.split("\\s+");
-                List<String>  in = new ArrayList<String>(Arrays.asList(input));
-                TreeNode root = deserialize(in);
-                return root;
-            }
-
-            private static TreeNode deserialize(List<String> in) {
-                if (in == null || in.size() == 0) return null;
-                String val = in.get(0); in.remove(0);
-                if (val.compareTo("#") == 0 || val.compareTo("") == 0)
+                if(data==null || data.length()==0)
                     return null;
-                TreeNode root = new TreeNode(Integer.valueOf(val));
-                root.left = deserialize(in);
-                root.right = deserialize(in);
+
+                String[] arr = data.split(",");
+                TreeNode root = new TreeNode(Integer.parseInt(arr[0]));
+
+
+                LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+                queue.add(root);
+
+                int i=1;
+                while(!queue.isEmpty()){
+                    TreeNode t = queue.poll();
+
+                    if(t==null)
+                        continue;
+
+                    if(!arr[i].equals("#")){
+                        t.left = new TreeNode(Integer.parseInt(arr[i]));
+                        queue.offer(t.left);
+
+                    }else{
+                        t.left = null;
+                        queue.offer(null);
+                    }
+                    i++;
+
+                    if(!arr[i].equals("#")){
+                        t.right = new TreeNode(Integer.parseInt(arr[i]));
+                        queue.offer(t.right);
+
+                    }else{
+                        t.right = null;
+                        queue.offer(null);
+                    }
+                    i++;
+                }
+
                 return root;
             }
 
@@ -65,6 +94,6 @@ public class SerializeDeserializeBTree {
         root.right = right;
 
         System.out.println("result1 = " + Solution.Codec.serialize(root));
-        System.out.println("result2 = " + Solution.Codec.deserialize("1 2 # # 3 # #"));
+        System.out.println("result2 = " + Solution.Codec.deserialize("1,2,3,#,#,#,#"));
     }
 }
