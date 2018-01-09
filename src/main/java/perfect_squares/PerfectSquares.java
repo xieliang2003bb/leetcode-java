2a41ed720159f29b010c750ba5fcf812a5b4deae
Leon Tse <leontse@hotmail.com>
2018-01-08 16:05:38 -0800

    add perfect_squares

---
+++ src/main/java/perfect_squares/PerfectSquares.java
@@ -0,0 +1,22 @@
+package perfect_squares;
+
+/**
+ * Created by lxie on 1/8/18.
+ */
+public class PerfectSquares {
+
+    public class Solution {
+        public int numSquares(int n) {
+            int[] dp = new int[n+1];
+            for (int i=0; i<dp.length; ++i) dp[i] = Integer.MAX_VALUE;
+            dp[0] = 0;
+            for (int i = 0; i <= n; ++i) {
+                for (int j = 1; i + j * j <= n; ++j) {
+                    dp[i + j * j] = Integer.min(dp[i + j * j], dp[i] + 1);
+                }
+            }
+            return dp[n];
+        }
+    }
+
+}
