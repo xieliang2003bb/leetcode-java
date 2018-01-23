8ef61f0c4ff0536f1f982f0627c2e32524a7d3df
Leon Tse <leontse@hotmail.com>
2018-01-23 10:50:08 -0800

    add number_of_islands i

--- src/main/java/number_of_islands/NumberOfIslands.java
+++ src/main/java/number_of_islands/NumberOfIslands.java
@@ -6,8 +6,32 @@ package number_of_islands;
 public class NumberOfIslands {
 
     public class Solution {
+        public int numIslands(char[][] grid) {
+            if (grid.length == 0 || grid[0].length == 0) return 0;
+            int m = grid.length, n = grid[0].length, res = 0;
+            boolean[][] visited = new boolean[m][n];
 
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j] == '1' && !visited[i][j]) {
+                        numIslandsDFS(grid, visited, i, j);
+                        ++res;
+                    }
+                }
+            }
+            return res;
+        }
 
+        public void numIslandsDFS(char[][] grid, boolean[][] visited, int x, int y) {
+            if (x < 0 || x >= grid.length) return;
+            if (y < 0 || y >= grid[0].length) return;
+            if (grid[x][y] != '1' || visited[x][y]) return;
+            visited[x][y] = true;
+            numIslandsDFS(grid, visited, x - 1, y);
+            numIslandsDFS(grid, visited, x + 1, y);
+            numIslandsDFS(grid, visited, x, y - 1);
+            numIslandsDFS(grid, visited, x, y + 1);
+        }
 
     }
 
