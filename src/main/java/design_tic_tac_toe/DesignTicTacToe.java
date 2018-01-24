e5cc419294e5dad0fb1ed4b36e2a618070720dd1
Leon Tse <leontse@hotmail.com>
2018-01-24 14:09:06 -0800

    design tic-tac-toe

---
+++ src/main/java/design_tic_tac_toe/DesignTicTacToe.java
@@ -0,0 +1,35 @@
+package design_tic_tac_toe;
+
+import static java.lang.StrictMath.abs;
+
+/**
+ * Created by lxie on 1/24/18.
+ */
+public class DesignTicTacToe {
+
+    public class Solution {
+
+        public class TicTacToe {
+            private final int N = 3;
+            private int[] rows = new int[N];
+            private int[] cols = new int[N];
+            private int diag = 0, rev_diag = 0;
+
+            public int move(int row, int col, int player) {
+                int add = player == 1 ? 1 : -1;
+                rows[row] += add; cols[col] += add;
+                diag += (row == col ? add : 0);
+                rev_diag += (row == N - col - 1 ? add : 0);
+                return (abs(rows[row]) == N || abs(cols[col]) == N || abs(diag) == N
+                        || abs(rev_diag) == N) ? player : 0;
+            }
+
+        }
+    }
+
+
+    public static class UnitTest {
+
+    }
+
+}
