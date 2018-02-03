3d0e81b923b2b6f0ad16b1a7b86de112249f568d
Leon Tse <leontse@hotmail.com>
2018-02-02 23:43:48 -0800

    modified reverse_words_in_a_string

--- src/main/java/reverse_words_in_a_string/ReverseWordsinaString.java
+++ src/main/java/reverse_words_in_a_string/ReverseWordsinaString.java
@@ -21,6 +21,27 @@ public class ReverseWordsinaString {
             }
             return r.toString();
         }
+
+        public String reverseWords1(String s0) {
+            int left = 0; char[] s = s0.toCharArray();
+            for (int i = 0; i <= s.length; ++i) {
+                if (i == s.length || s[i] == ' ') {
+                    reverse(s, left, i - 1);
+                    left = i + 1;
+                }
+            }
+            return new String(reverse(s, 0, s.length - 1));
+        }
+
+        public char[] reverse(char[] s, int left, int right) {
+            while (left < right) {
+                char t = s[left];
+                s[left] = s[right];
+                s[right] = t;
+                ++left; --right;
+            }
+            return s;
+        }
     }
 
     public static class UnitTest {
