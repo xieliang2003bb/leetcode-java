1a020d144bd740f1d985dc08349cfd948d45086a
Leon Tse <leontse@hotmail.com>
2018-02-11 23:55:58 -0800

    modify bitwise_and_of_all_numbers

--- src/main/java/bitwise_and_of_numbers_range/BitwiseANDofNumbersRange.java
+++ src/main/java/bitwise_and_of_numbers_range/BitwiseANDofNumbersRange.java
@@ -4,15 +4,12 @@ public class BitwiseANDofNumbersRange {
 
     public class Solution {
         public int rangeBitwiseAnd(int m, int n) {
-            int r = 0;
-            for (int i = 0; i < 32 && n != 0; i++) {
-                if ((m & 1) != 0 && n == m) {
-                    r |= (1 << i);
-                }
-                n >>>= 1;
-                m >>>= 1;
+            int d = Integer.MAX_VALUE;
+            while ((m & d) != (n & d)) {
+                d <<= 1;
             }
-            return r;
+            return m & d;
+
         }
     }
 }
