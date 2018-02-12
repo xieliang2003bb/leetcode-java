372ad40034abe4b51764d40c559775beba77d69b
Leon Tse <leontse@hotmail.com>
2018-02-11 23:07:01 -0800

    modify reverse_bits

--- src/main/java/reverse_bits/ReverseBits.java
+++ src/main/java/reverse_bits/ReverseBits.java
@@ -3,18 +3,13 @@ package reverse_bits;
 public class ReverseBits {
 
     public class Solution {
-
         public int reverseBits(int n) {
-            int reversed = 0;
-            int i;
-            for (i = 32; i > 0 && n != 0; i--) {
-                reversed = (reversed << 1) + (n & 1);
-                n >>= 1;
+            int res = 0;
+            for (int i = 0; i < 32; ++i) {
+                res |= ((n >> i) & 1) << (31 - i);
             }
-            reversed <<= i;
-            return reversed;
+            return res;
         }
     }
-
 }
 
