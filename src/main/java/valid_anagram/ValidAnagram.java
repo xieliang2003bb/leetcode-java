0c59ffcb817fcc432713605110b4be386d4f4ef3
Leon Tse <leontse@hotmail.com>
2018-03-07 10:14:38 -0800

    add shortest_word_distance

--- src/main/java/valid_anagram/ValidAnagram.java
+++ src/main/java/valid_anagram/ValidAnagram.java
@@ -7,10 +7,11 @@ public class ValidAnagram {
 
     public class Solution {
         public boolean isAnagram(String s, String t) {
-            if (s.length() != t.length()) return false;
+            int lens = s.length(), lent = t.length();
+            if (lens != lent) return false;
             int[] m = new int[26];
-            for (int i = 0; i < s.length(); ++i) ++m[s.toCharArray()[i] - 'a'];
-            for (int i = 0; i < t.length(); ++i) {
+            for (int i = 0; i < lens; ++i) ++m[s.toCharArray()[i] - 'a'];
+            for (int i = 0; i < lent; ++i) {
                 if (--m[t.toCharArray()[i] - 'a'] < 0) return false;
             }
             return true;
