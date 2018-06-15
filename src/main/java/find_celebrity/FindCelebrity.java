cd331be04581f245e28e08b6ac721a5cfc3f511a
Leon Tse <leontse@hotmail.com>
2018-06-14 22:59:54 -0700

    find_celerity

---
+++ src/main/java/find_celebrity/FindCelebrity.java
@@ -0,0 +1,29 @@
+package find_celebrity;
+
+/**
+ * Created by lxie on 6/14/18.
+ */
+public class FindCelebrity {
+
+    public class Solution {
+
+        private boolean knows(int a, int b) {
+            return true;  // it varies
+        }
+
+        public int findCelebrity(int n) {
+            int res = 0;
+            for (int i = 0; i < n; ++i) {
+                if (knows(res, i)) res = i; // get a candidate
+            }
+            for (int i = 0; i < n; ++i) {
+                if (res != i && (knows(res, i) || !knows(i, res))) return -1;
+            }
+            return res;
+        }
+    }
+
+    public class UnitTest {
+
+    }
+}
