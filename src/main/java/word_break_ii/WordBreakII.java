060fd1b33369ba3032e9681bff90718892f6f255
Leon Tse <leontse@hotmail.com>
2018-07-30 23:24:42 -0700

    modify word_break_ii

--- src/main/java/word_break_ii/WordBreakII.java
+++ src/main/java/word_break_ii/WordBreakII.java
@@ -1,43 +1,55 @@
 package word_break_ii;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class WordBreakII {
 
-    public class Solution {
-        private ArrayList<String> searchWordBreak(String s, Set<String> dict,
-                Map<String, ArrayList<String>> solutions) {
-            ArrayList<String> solution = solutions.get(s);
-            if (solution != null) {
-                return solution;
-            }
-            solution = new ArrayList<String>();
-            for (int i = 1; i < s.length(); i++) {
-                String prefix = s.substring(0, i);
-                if (dict.contains(prefix)) {
-                    for (String subfix : searchWordBreak(s.substring(i), dict,
-                            solutions)) {
-                        solution.add(prefix + " " + subfix);
-                    }
-                }
-            }
-            if (dict.contains(s)) {
-                solution.add(s);
-            }
-            solutions.put(s, solution);
-            return solution;
+    public static class Solution {
+        public ArrayList<String> wordBreak(String s, Set<String> dict) {
+            Map<String, ArrayList<String>> memo = new HashMap<String, ArrayList<String>>();
+            return wordBreakHelper(s, dict, memo);
         }
 
-        public ArrayList<String> wordBreak(String s, Set<String> dict) {
-            return searchWordBreak(s, dict,
-                    new HashMap<String, ArrayList<String>>());
+        public ArrayList<String> wordBreakHelper(String s,
+                                                 Set<String> dict,
+                                                 Map<String, ArrayList<String>> memo){
+            if (memo.containsKey(s)) {
+                return memo.get(s);
+            }
+
+            ArrayList<String> results = new ArrayList<String>();
+
+            if (s.length() == 0) {
+                return results;
+            }
+
+            if (dict.contains(s)) {
+                results.add(s);
+            }
+
+            for (int len = 1; len < s.length(); ++len){
+                String word = s.substring(0, len);
+                if (!dict.contains(word)) {
+                    continue;
+                }
+
+                String suffix = s.substring(len);
+                ArrayList<String> segmentations = wordBreakHelper(suffix, dict, memo);
+
+                for (String segmentation: segmentations){
+                    results.add(word + " " + segmentation);
+                }
+            }
+
+            memo.put(s, results);
+            return results;
         }
     }
 
-    public static class UnitTest {
-
+    public static void main(String[] args) {
+        Set<String> dict = new HashSet<>(Arrays.asList("cat", "cats", "and", "sand", "dog"));
+        Solution sol = new Solution();
+        System.out.println(sol.wordBreak("catsanddog", dict));
+        
     }
 }
