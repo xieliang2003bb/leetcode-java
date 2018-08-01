5faeab985f3eea240dedde9fc89dc3000cfaac84
Leon Tse <leontse@hotmail.com>
2018-08-01 10:16:16 -0700

    modify palindrome_partitioning

--- src/main/java/palindrome_partitioning/PalindromePartitioning.java
+++ src/main/java/palindrome_partitioning/PalindromePartitioning.java
@@ -1,51 +1,46 @@
 package palindrome_partitioning;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 public class PalindromePartitioning {
 
-    public class Solution {
-        private boolean isPalindromic(String s) {
-            for (int i = 0; i < s.length() / 2; i++) {
-                if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
-                    return false;
+    public static class Solution {
+
+        public List<List<String>> partition(String s) {
+            List<List<String>> res = new ArrayList<>();
+            List<String> out = new ArrayList<>();
+            partitionDFS(s, 0, out, res);
+            return res;
+        }
+
+        public void partitionDFS(String s, int start, List<String> out, List<List<String>> res) {
+            if (start == s.length()) {
+                res.add(new ArrayList<>(out));
+                return;
+            }
+            for (int i = start; i < s.length(); ++i) {
+                if (isPalindrome(s, start, i)) {
+                    out.add(s.substring(start, i+1));
+                    partitionDFS(s, i + 1, out, res);
+                    out.remove(out.size()-1);
                 }
+            }
+        }
+
+        private boolean isPalindrome(String s, int start, int end) {
+            while (start < end) {
+                if (s.charAt(start) != s.charAt(end)) return false;
+                ++start;
+                --end;
             }
             return true;
         }
 
-        private Map<String, ArrayList<ArrayList<String>>> cache = new HashMap<String, ArrayList<ArrayList<String>>>();
-
-        public ArrayList<ArrayList<String>> partition(String s) {
-            ArrayList<ArrayList<String>> ans = cache.get(s);
-            if (ans != null) {
-                return ans;
-            }
-            ans = new ArrayList<ArrayList<String>>();
-            for (int i = 1; i < s.length(); i++) {
-                String prefix = s.substring(0, i);
-                if (isPalindromic(prefix)) {
-                    for (ArrayList<String> subans : partition(s.substring(i))) {
-                        ArrayList<String> temp = new ArrayList<String>();
-                        temp.add(prefix);
-                        temp.addAll(subans);
-                        ans.add(temp);
-                    }
-                }
-            }
-            if (isPalindromic(s)) {
-                ArrayList<String> temp = new ArrayList<String>();
-                temp.add(s);
-                ans.add(temp);
-            }
-            cache.put(s, ans);
-            return ans;
-        }
     }
 
-    public static class UnitTest {
-
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.partition("aab"));
     }
 }
