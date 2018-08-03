922606eee07fc193cb99492beb32cc7e33573db8
Leon Tse <leontse@hotmail.com>
2018-08-03 07:26:11 -0700

    modify binary_tree_maximum_path_sum.

--- src/main/java/binary_tree_maximum_path_sum/BinaryTreeMaximumPathSum.java
+++ src/main/java/binary_tree_maximum_path_sum/BinaryTreeMaximumPathSum.java
@@ -5,36 +5,21 @@ import common.TreeNode;
 public class BinaryTreeMaximumPathSum {
 
     public class Solution {
-        private int maxPathSum(TreeNode root, int[] partSum) {
-            int leftSum = 0;
-            int leftPartSum = 0;
-            int rightSum = 0;
-            int rightPartSum = 0;
-            if (root.left != null) {
-                leftSum = maxPathSum(root.left, partSum);
-                leftPartSum = partSum[0] > 0 ? partSum[0] : 0;
-            }
-            if (root.right != null) {
-                rightSum = maxPathSum(root.right, partSum);
-                rightPartSum = partSum[0] > 0 ? partSum[0] : 0;
-            }
-            int maxSum = leftPartSum + rightPartSum + root.val;
-            if (root.left != null) {
-                maxSum = Math.max(maxSum, leftSum);
-            }
-            if (root.right != null) {
-                maxSum = Math.max(maxSum, rightSum);
-            }
-            partSum[0] = Math.max(leftPartSum, rightPartSum) + root.val;
-            return maxSum;
-        }
 
         public int maxPathSum(TreeNode root) {
-            if (root == null) {
-                return 0;
-            }
-            return maxPathSum(root, new int[1]);
+            int[] res = {Integer.MIN_VALUE};
+            helper(root, res);
+            return res[0];
         }
+
+        private int helper(TreeNode node, int[] res) {
+            if (node == null) return 0;
+            int left = Math.max(helper(node.left, res), 0);
+            int right = Math.max(helper(node.right, res), 0);
+            res[0] = Math.max(res[0], left + right + node.val);
+            return Math.max(left, right) + node.val;
+        }
+
     }
 
     public static class UnitTest {
