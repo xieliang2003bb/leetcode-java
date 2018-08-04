d4b87acbf943b9613d2a3f1e72d4be40bef97e72
Leon Tse <leontse@hotmail.com>
2018-08-04 10:30:59 -0700

    modify convert_sorted_list_to_bst

--- src/main/java/convert_sorted_list_to_binary_search_tree/ConvertSortedListtoBinarySearchTree.java
+++ src/main/java/convert_sorted_list_to_binary_search_tree/ConvertSortedListtoBinarySearchTree.java
@@ -6,29 +6,25 @@ import common.TreeNode;
 public class ConvertSortedListtoBinarySearchTree {
 
     public class Solution {
-        private TreeNode sortedListToBST(ListNode head, ListNode[] tail,
-                int begin, int end) {
-            if (begin > end) {
-                tail[0] = head;
-                return null;
+        public TreeNode sortedListToBST(ListNode head) {
+            if (head == null) return null;
+            if (head.next == null) return new TreeNode(head.val);
+            ListNode slow = head;
+            ListNode fast = head;
+            ListNode last = slow;
+            while (fast.next != null && fast.next.next != null) {
+                last = slow;
+                slow = slow.next;
+                fast = fast.next.next;
             }
-            TreeNode p = new TreeNode(0);
-            int mid = begin + (end - begin) / 2;
-            p.left = sortedListToBST(head, tail, begin, mid - 1);
-            p.val = tail[0].val;
-            p.right = sortedListToBST(tail[0].next, tail, mid + 1, end);
-            return p;
+            fast = slow.next;
+            last.next = null;
+            TreeNode cur = new TreeNode(slow.val);
+            if (head != slow) cur.left = sortedListToBST(head);
+            cur.right = sortedListToBST(fast);
+            return cur;
         }
 
-        public TreeNode sortedListToBST(ListNode head) {
-            ListNode p = head;
-            int len = 0;
-            while (p != null) {
-                len++;
-                p = p.next;
-            }
-            return sortedListToBST(head, new ListNode[1], 0, len - 1);
-        }
     }
 
     public static class UnitTest {
