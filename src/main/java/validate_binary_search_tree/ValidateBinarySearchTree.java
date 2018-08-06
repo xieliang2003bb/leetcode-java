17836762c0c3527a911190087aa2df7b268ff317
Leon Tse <leontse@hotmail.com>
2018-08-05 22:35:15 -0700

    modify validate_bst

--- src/main/java/validate_binary_search_tree/ValidateBinarySearchTree.java
+++ src/main/java/validate_binary_search_tree/ValidateBinarySearchTree.java
@@ -5,31 +5,15 @@ import common.TreeNode;
 public class ValidateBinarySearchTree {
 
     public class Solution {
-        private boolean isValidBST(TreeNode root, int[] minMax) {
-            int min = root.val;
-            int max = root.val;
-            if (root.left != null) {
-                if (!isValidBST(root.left, minMax) || root.val <= minMax[1]) {
-                    return false;
-                }
-                min = minMax[0];
-            }
-            if (root.right != null) {
-                if (!isValidBST(root.right, minMax) || root.val >= minMax[0]) {
-                    return false;
-                }
-                max = minMax[1];
-            }
-            minMax[0] = min;
-            minMax[1] = max;
-            return true;
-        }
 
         public boolean isValidBST(TreeNode root) {
-            if (root == null) {
-                return true;
-            }
-            return isValidBST(root, new int[2]);
+            return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        }
+
+        private boolean isValidBST(TreeNode root, long mn, long mx) {
+            if (root == null) return true;
+            if (root.val <= mn || root.val >= mx) return false;
+            return isValidBST(root.left, mn, root.val) && isValidBST(root.right, root.val, mx);
         }
     }
 
