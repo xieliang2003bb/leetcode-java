79e3912777ab6768aa5adca2b3e3992a338577a8
Leon Tse <leontse@hotmail.com>
2018-08-05 23:31:27 -0700

    interleaving_string

--- src/main/java/interleaving_string/InterleavingString.java
+++ src/main/java/interleaving_string/InterleavingString.java
@@ -4,21 +4,26 @@ public class InterleavingString {
 
     public class Solution {
         public boolean isInterleave(String s1, String s2, String s3) {
-            if (s1.length() + s2.length() != s3.length()) {
-                return false;
+            if (s1.length() + s2.length() != s3.length()) return false;
+            int n1 = s1.length();
+            int n2 = s2.length();
+            boolean[][] dp = new boolean[n1+1][n2+1];
+            dp[0][0] = true;
+            for (int i = 1; i <= n1; ++i) {
+                dp[i][0] = dp[i - 1][0] && (s1.charAt(i - 1) == s3.charAt(i - 1));
             }
-            boolean[] dp = new boolean[s2.length() + 1];
-            for (int i = 0; i <= s1.length(); i++) {
-                for (int j = 0; j <= s2.length(); j++) {
-                    dp[j] = (i == 0 && j == 0)
-                            || (i != 0 && dp[j] && s1.charAt(i - 1) == s3
-                                    .charAt(i + j - 1))
-                            || (j != 0 && dp[j - 1] && s2.charAt(j - 1) == s3
-                                    .charAt(i + j - 1));
+            for (int i = 1; i <= n2; ++i) {
+                dp[0][i] = dp[0][i - 1] && (s2.charAt(i - 1) == s3.charAt(i - 1));
+            }
+            for (int i = 1; i <= n1; ++i) {
+                for (int j = 1; j <= n2; ++j) {
+                    dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i - 1 + j)) ||
+                            (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1 + i));
                 }
             }
-            return dp[s2.length()];
+            return dp[n1][n2];
         }
+
     }
 
     public static class UnitTest {
