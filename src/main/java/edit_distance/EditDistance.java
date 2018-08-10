1311ce3bb9d5adfd0810d09b69b19dcb2d6853c5
Leon Tse <leontse@hotmail.com>
2018-08-09 22:28:21 -0700

    modify edit_distance

--- src/main/java/edit_distance/EditDistance.java
+++ src/main/java/edit_distance/EditDistance.java
@@ -4,23 +4,20 @@ public class EditDistance {
 
     public class Solution {
         public int minDistance(String word1, String word2) {
-            assert word1 != null && word2 != null;
-            int[][] dp = new int[word1.length() + 1][word2.length() + 1];
-            for (int i = 0; i <= word2.length(); i++) {
-                dp[0][i] = i;
-            }
-            for (int i = 1; i <= word1.length(); i++) {
-                dp[i][0] = i;
-                for (int j = 1; j <= word2.length(); j++) {
-                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
-                    if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                        dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]);
+            int n1 = word1.length(), n2 = word2.length();
+            int[][] dp = new int[n1 + 1][n2 + 1];
+            for (int i = 0; i <= n1; ++i) dp[i][0] = i;
+            for (int i = 0; i <= n2; ++i) dp[0][i] = i;
+            for (int i = 1; i <= n1; ++i) {
+                for (int j = 1; j <= n2; ++j) {
+                    if (word1.charAt(i-1) == word2.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1];
                     } else {
-                        dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1] + 1);
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                     }
                 }
             }
-            return dp[word1.length()][word2.length()];
+            return dp[n1][n2];
         }
     }
 
