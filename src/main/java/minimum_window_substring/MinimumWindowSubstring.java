5d92c416da895e8d0a4d87dab1376fc693bf2c3d
Leon Tse <leontse@hotmail.com>
2018-08-09 18:40:13 -0700

    modify minimum window substring

--- src/main/java/minimum_window_substring/MinimumWindowSubstring.java
+++ src/main/java/minimum_window_substring/MinimumWindowSubstring.java
@@ -4,57 +4,35 @@ public class MinimumWindowSubstring {
 
     public class Solution {
         public String minWindow(String S, String T) {
-            int[] tCount = new int[256];
-            for (int i = 0; i < T.length(); i++) {
-                tCount[T.charAt(i)]++;
-            }
-            int[] sCount = new int[256];
-            int i = 0;
-            for (; i < S.length(); i++) {
-                sCount[S.charAt(i)]++;
-                boolean find = true;
-                for (int j = 0; j < 256; j++) {
-                    if (sCount[j] < tCount[j]) {
-                        find = false;
-                        break;
+            int[] need = new int[256];
+            for(int i = 0; i < T.length(); i++)
+                need[T.charAt(i)]++;
+            int[] found = new int[256];
+
+            int count = 0;
+            int minLen = S.length()+1;
+            int minBegin = 0;
+            for(int begin = 0, end = 0; end < S.length(); ++end){
+                char ch = S.charAt(end);
+                if(need[ch] == 0) continue;
+                if(++found[ch] <= need[ch]) count++;
+                if(count == T.length()){
+                    while(need[S.charAt(begin)] == 0 ||
+                            found[S.charAt(begin)] > need[S.charAt(begin)]){
+                        if(found[S.charAt(begin)] > need[S.charAt(begin)])
+                            found[S.charAt(begin)]--;
+                        begin++;
+                    }
+                    int leng = end - begin + 1;
+                    if(leng < minLen){
+                        minLen = leng;
+                        minBegin = begin;
                     }
                 }
-                if (find) {
-                    break;
-                }
             }
-            if (i == S.length()) {
-                return "";
-            }
-            int windowStart = 0;
-            int windowEnd = i;
-            int minWindowStart = windowStart;
-            int minWindowEnd = windowEnd;
-            while (windowStart < S.length()) {
-                char c = S.charAt(windowStart);
-                sCount[c]--;
-                windowStart++;
-                if (sCount[c] < tCount[c]) {
-                    windowEnd++;
-                    while (windowEnd < S.length()) {
-                        char endC = S.charAt(windowEnd);
-                        sCount[endC]++;
-                        if (c == endC) {
-                            break;
-                        }
-                        windowEnd++;
-                    }
-                    if (windowEnd == S.length()) {
-                        break;
-                    }
-                }
-                if (windowEnd - windowStart < minWindowEnd - minWindowStart) {
-                    minWindowStart = windowStart;
-                    minWindowEnd = windowEnd;
-                }
-            }
-            return S.substring(minWindowStart, minWindowEnd + 1);
+            return minLen > S.length()?"":S.substring(minBegin,minBegin+minLen);
         }
+
     }
 
     public static class UnitTest {
