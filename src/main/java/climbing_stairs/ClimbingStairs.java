216d941a9fcfc73ce0c5115e2e0e9e4135ec1866
Leon Tse <leontse@hotmail.com>
2018-08-10 08:03:36 -0700

    modify multiple files.

--- src/main/java/climbing_stairs/ClimbingStairs.java
+++ src/main/java/climbing_stairs/ClimbingStairs.java
@@ -4,15 +4,15 @@ public class ClimbingStairs {
 
     public class Solution {
         public int climbStairs(int n) {
-            int f1 = 1;
-            int f2 = 1;
-            for (int i = 2; i <= n; i++) {
-                int temp = f1 + f2;
-                f1 = f2;
-                f2 = temp;
+            if (n <= 1) return 1;
+            int[] dp = new int[n];
+            dp[0] = 1; dp[1] = 2;
+            for (int i = 2; i < n; ++i) {
+                dp[i] = dp[i - 1] + dp[i - 2];
             }
-            return f2;
+            return dp[n-1];
         }
+
     }
 
     public static class UnitTest {
--- src/main/java/sqrt_x/SqrtX.java
+++ src/main/java/sqrt_x/SqrtX.java
@@ -3,13 +3,19 @@ package sqrt_x;
 public class SqrtX {
 
     public class Solution {
-        public int sqrt(int a) {
-            double x = 1.0;
-            while (Math.abs(x * x - a) > 10E-6) {
-                x = (x + a / x) / 2;
+
+        public int SqrtX(int x) {
+        long left = 0, right = (x / 2) + 1;
+            while (left <= right) {
+            long mid = (left + right) / 2;
+            long sq = mid * mid;
+                if (sq == x) return (int) mid;
+                else if (sq < x) left = mid + 1;
+                else right = mid - 1;
             }
-            return (int) x;
+            return (int) right;
         }
+
     }
 
     public static class UnitTest {
