28310976ca1dc1a63fc6cd7b9fced86bb2296a46
Leon Tse <leontse@hotmail.com>
2018-08-10 22:39:08 -0700

    modify unique_paths_with_obstacle

--- src/main/java/unique_paths_ii/UniquePathsII.java
+++ src/main/java/unique_paths_ii/UniquePathsII.java
@@ -4,18 +4,20 @@ public class UniquePathsII {
 
     public class Solution {
         public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-            int m = obstacleGrid.length;
-            int n = obstacleGrid[0].length;
+            if (obstacleGrid.length == 0 || obstacleGrid[0].length == 0) return 0;
+            int m = obstacleGrid.length, n = obstacleGrid[0].length;
+            if (obstacleGrid[0][0] == 1) return 0;
             int[] dp = new int[n];
-            dp[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
-            for (int i = 0; i < m; i++) {
-                dp[0] = obstacleGrid[i][0] == 1 ? 0 : dp[0];
-                for (int j = 1; j < n; j++) {
-                    dp[j] = obstacleGrid[i][j] == 1 ? 0 : dp[j - 1] + dp[j];
+            dp[0] = 1;
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (obstacleGrid[i][j] == 1) dp[j] = 0;
+                    else if (j > 0) dp[j] += dp[j - 1];  //rolling array
                 }
             }
             return dp[n - 1];
         }
+
     }
 
     public static class UnitTest {
