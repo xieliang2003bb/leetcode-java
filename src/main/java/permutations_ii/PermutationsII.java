524cbc00322f4bdbfec252aa80ae14df5dc728d5
Leon Tse <leontse@hotmail.com>
2018-08-12 15:30:03 -0700

    modify permutation_ii

--- src/main/java/permutations_ii/PermutationsII.java
+++ src/main/java/permutations_ii/PermutationsII.java
@@ -1,58 +1,34 @@
 package permutations_ii;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PermutationsII {
 
     public class Solution {
+        public List<List<Integer>> permuteUnique(int[] nums) {
+            Set<List<Integer>> res = new HashSet<>();
+            permute(nums, 0, res);
+            return new ArrayList<>(res);
+        }
+
+        private void permute(int[] nums, int start, Set<List<Integer>> res) {
+            if (start >= nums.length)
+                res.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+            for (int i = start; i < nums.length; ++i) {
+                if (i != start && nums[i] == nums[start]) continue;
+                swap(nums, i, start);
+                permute(nums, start + 1, res);
+                swap(nums, i, start);
+            }
+        }
+
         private void swap(int[] num, int i, int j) {
             int temp = num[i];
             num[i] = num[j];
             num[j] = temp;
         }
 
-        private void reverse(int[] num, int begin, int end) {
-            end--;
-            while (begin < end) {
-                swap(num, begin++, end--);
-            }
-        }
-
-        private boolean nextPermutation(int[] num) {
-            if (num.length <= 1) {
-                return false;
-            }
-            int i = num.length - 1;
-            while (true) {
-                i--;
-                if (num[i] < num[i + 1]) {
-                    int j = num.length;
-                    while (num[i] >= num[--j]) {
-                    }
-                    swap(num, i, j);
-                    reverse(num, i + 1, num.length);
-                    return true;
-                }
-                if (i == 0) {
-                    reverse(num, 0, num.length);
-                    return false;
-                }
-            }
-        }
-
-        public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
-            Arrays.sort(num);
-            ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-            do {
-                ArrayList<Integer> l = new ArrayList<Integer>();
-                for (int n : num) {
-                    l.add(n);
-                }
-                ans.add(l);
-            } while (nextPermutation(num));
-            return ans;
-        }
     }
 
     public static class UnitTest {
