455c20559b69c947e4d13b850b2d8f97eabad9eb
Leon Tse <leontse@hotmail.com>
2018-08-13 00:08:54 -0700

    modify first_missing_positive

--- src/main/java/first_missing_positive/FirstMissingPositive.java
+++ src/main/java/first_missing_positive/FirstMissingPositive.java
@@ -2,30 +2,26 @@ package first_missing_positive;
 
 public class FirstMissingPositive {
 
-    public class Solution {
-        public int firstMissingPositive(int[] A) {
-            int n = A.length;
-            for (int i = 0; i < n; i++) {
-                if (A[i] <= 0) {
-                    A[i] = n + 1;
-                }
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int tmp = nums[i];
+                nums[i] = nums[nums[i] - 1];
+                nums[tmp - 1] = tmp;
             }
-            for (int i = 0; i < n; i++) {
-                int temp = Math.abs(A[i]);
-                if (temp <= n && A[temp - 1] > 0) {
-                    A[temp - 1] = -A[temp - 1];
-                }
-            }
-            for (int i = 0; i < n; i++) {
-                if (A[i] > 0) {
-                    return i + 1;
-                }
-            }
-            return n + 1;
         }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return n + 1;
     }
 
-    public static class UnitTest {
+    public static void main(String[] args) {
+        FirstMissingPositive f =new FirstMissingPositive();
+        int[] a = {3,4,-1,1};
+        System.out.println(f.firstMissingPositive(a));
+
 
     }
 }
