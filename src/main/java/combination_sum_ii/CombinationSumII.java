6cc40c20ad56a95901cc567cbf5022b87b1a492a
Leon Tse <leontse@hotmail.com>
2018-08-13 00:21:25 -0700

    modify combination_sum_ii

--- src/main/java/combination_sum_ii/CombinationSumII.java
+++ src/main/java/combination_sum_ii/CombinationSumII.java
@@ -1,6 +1,5 @@
 package combination_sum_ii;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,52 +7,27 @@ import java.util.List;
 public class CombinationSumII {
 
     public class Solution {
-        private void search(List<Integer> nums, List<Integer> counts,
-                int index, int target, ArrayDeque<Integer> s,
-                ArrayList<ArrayList<Integer>> ans) {
-            if (target == 0) {
-                ans.add(new ArrayList<Integer>(s));
-            }
-            if (target <= 0 || index == nums.size()) {
-                return;
-            }
-            int n = nums.get(index);
-            int count = counts.get(index);
-            for (int i = 0; i <= count; i++) {
-                search(nums, counts, index + 1, target - i * n, s, ans);
-                s.offerLast(n);
-            }
-            for (int i = 0; i <= count; i++) {
-                s.removeLast();
-            }
-        }
-
-        public ArrayList<ArrayList<Integer>> combinationSum2(int[] num,
-                int target) {
-            ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-            if (num.length == 0) {
-                return ans;
-            }
+        public List<List<Integer>> combinationSum2(int[] num, int target) {
+            List<List<Integer>> res = new ArrayList<>();
+            List<Integer> out = new ArrayList<>();
             Arrays.sort(num);
-            List<Integer> nums = new ArrayList<Integer>();
-            List<Integer> counts = new ArrayList<Integer>();
-            int count = 1;
-            int i = 1;
-            for (; i < num.length; i++) {
-                if (num[i] != num[i - 1]) {
-                    nums.add(num[i - 1]);
-                    counts.add(count);
-                    count = 1;
-                } else {
-                    count++;
-                }
-
-            }
-            nums.add(num[i - 1]);
-            counts.add(count);
-            search(nums, counts, 0, target, new ArrayDeque<Integer>(), ans);
-            return ans;
+            combinationSum2DFS(num, target, 0, out, res);
+            return res;
         }
+
+        private void combinationSum2DFS(int[] num, int target, int start, List<Integer> out, List<List<Integer>> res) {
+            if (target < 0) return;
+            else if (target == 0) res.add(new ArrayList<>(out));
+            else {
+                for (int i = start; i < num.length; ++i) {
+                    if (i > start && num[i] == num[i - 1]) continue;
+                    out.add(num[i]);
+                    combinationSum2DFS(num, target - num[i], i + 1, out, res); // no duplicate
+                    out.remove(out.size()-1);
+                }
+            }
+        }
+
     }
 
     public static class UnitTest {
