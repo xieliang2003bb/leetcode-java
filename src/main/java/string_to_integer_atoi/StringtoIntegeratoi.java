06fe5d3a831809ec0edccebc31198d4d39f939e3
Leon Tse <leontse@hotmail.com>
2018-08-15 07:56:50 -0700

    modify atoi

--- src/main/java/string_to_integer_atoi/StringtoIntegeratoi.java
+++ src/main/java/string_to_integer_atoi/StringtoIntegeratoi.java
@@ -5,40 +5,21 @@ public class StringtoIntegeratoi {
     public class Solution {
 
         public int atoi(String str) {
-            int index = 0;
-            while (index < str.length()
-                    && Character.isWhitespace(str.charAt(index))) {
-                index++;
+            if (str.isEmpty()) return 0;
+            int sign = 1, base = 0, i = 0, n = str.length();
+            while (i < n && str.charAt(i) == ' ') ++i;
+            if (str.charAt(i) == '+' || str.charAt(i) == '-') {
+                sign = (str.charAt(i++) == '+') ? 1 : -1;
             }
-            if (index == str.length()) {
-                return 0;
-            }
-            boolean negative = false;
-            if (str.charAt(index) == '+') {
-                index++;
-            } else if (str.charAt(index) == '-') {
-                index++;
-                negative = true;
-            }
-            long ans = 0;
-            while (index < str.length() && Character.isDigit(str.charAt(index))) {
-                ans = ans * 10 + str.charAt(index) - '0';
-                if (ans > Integer.MAX_VALUE + 1L) {
-                    break;
+            while (i < n && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                if (base > Integer.MAX_VALUE / 10 || (base == Integer.MAX_VALUE / 10 &&
+                        str.charAt(i) - '0' > 7)) {
+                    return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
                 }
-                index++;
+                base = 10 * base + (str.charAt(i++) - '0');
             }
-            if (negative) {
-                ans = -ans;
-                if (ans < Integer.MIN_VALUE) {
-                    return Integer.MIN_VALUE;
-                }
-            } else {
-                if (ans > Integer.MAX_VALUE) {
-                    return Integer.MAX_VALUE;
-                }
-            }
-            return (int) ans;
+            return base * sign;
+
         }
     }
 
