cc5638fe8f52386a1ff62eb7bacbc0a78ef948de
Leon Tse <leontse@hotmail.com>
2018-08-15 12:36:40 -0700

    modify median_of_two_sorted_arrays

--- src/main/java/median_of_two_sorted_arrays/MedianofTwoSortedArrays.java
+++ src/main/java/median_of_two_sorted_arrays/MedianofTwoSortedArrays.java
@@ -2,43 +2,29 @@ package median_of_two_sorted_arrays;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class MedianofTwoSortedArrays {
 
     public class Solution {
 
-        private int findKth(int A[], int m, int B[], int n, int k) {
-            if (m == 0) {
-                return B[k - 1];
-            }
-            if (n == 0) {
-                return A[k - 1];
-            }
-            if (m + n == k) {
-                return Math.max(A[m - 1], B[n - 1]);
-            }
-            if (m > n) {
-                return findKth(B, n, A, m, k);
-            }
-            int x = Math.min(m, k / 2 + 1);
-            int y = k + 1 - x;
-            if (A[x - 1] < B[y - 1]) {
-                return findKth(A, m, B, y - 1, k);
-            } else if (A[x - 1] > B[y - 1]) {
-                return findKth(A, x - 1, B, n, k);
-            } else {
-                return A[x - 1];
-            }
+        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+            int m = nums1.length, n = nums2.length, left = (m + n + 1) / 2, right = (m + n + 2) / 2;
+            return (findKth(nums1, nums2, left) + findKth(nums1, nums2, right)) / 2.0;
         }
-
-        public double findMedianSortedArrays(int A[], int B[]) {
-            int n = A.length + B.length;
-            if (n % 2 == 1) {
-                return findKth(A, A.length, B, B.length, n / 2 + 1);
+        int findKth(int[] nums1, int[] nums2, int k) {
+            int m = nums1.length, n = nums2.length;
+            if (m > n) return findKth(nums2, nums1, k);
+            if (m == 0) return nums2[k - 1];
+            if (k == 1) return Math.min(nums1[0], nums2[0]);
+            int i = Math.min(m, k / 2), j = Math.min(n, k / 2);
+            if (nums1[i - 1] > nums2[j - 1]) {
+                return findKth(nums1, Arrays.copyOfRange(nums2, j, n), k - j);
+            } else {
+                return findKth(Arrays.copyOfRange(nums1, i, m), nums2, k - i);
             }
-            return (findKth(A, A.length, B, B.length, n / 2) +
-                    findKth(A, A.length, B, B.length, n / 2 + 1)) / 2.0;
         }
     }
 
