5bef38ad9d7b472d51ecda6794e6534e46d9ad0b
Leon Tse <leontse@hotmail.com>
2018-08-15 06:59:45 -0700

    modify roman_to_integer

--- src/main/java/roman_to_integer/RomantoInteger.java
+++ src/main/java/roman_to_integer/RomantoInteger.java
@@ -6,30 +6,25 @@ import java.util.Map;
 public class RomantoInteger {
 
     public class Solution {
-        private Map<Character, Integer> symbols = new HashMap<Character, Integer>();
+        private Map<Character, Integer> m = new HashMap<Character, Integer>();
         {
-            symbols.put('M', 1000);
-            symbols.put('D', 500);
-            symbols.put('C', 100);
-            symbols.put('L', 50);
-            symbols.put('X', 10);
-            symbols.put('V', 5);
-            symbols.put('I', 1);
+            m.put('M', 1000);
+            m.put('D', 500);
+            m.put('C', 100);
+            m.put('L', 50);
+            m.put('X', 10);
+            m.put('V', 5);
+            m.put('I', 1);
         }
 
         public int romanToInt(String s) {
-            int num = 0;
-            num += symbols.get(s.charAt(s.length() - 1));
-            for (int i = s.length() - 2; i >= 0; i--) {
-                char c = s.charAt(i);
-                char nextC = s.charAt(i + 1);
-                if (symbols.get(c) < symbols.get(nextC)) {
-                    num -= symbols.get(c);
-                } else {
-                    num += symbols.get(c);
-                }
+            int res = 0;
+            for (int i = 0; i < s.length(); ++i) {
+                int val = m.get(s.charAt(i));
+                if (i == s.length() - 1 || m.get(s.charAt(i+1)) <= m.get(s.charAt(i))) res += val;
+                else res -= val;
             }
-            return num;
+            return res;
         }
     }
 
