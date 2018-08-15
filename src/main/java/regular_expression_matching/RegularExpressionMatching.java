7d68b7879119157817910b46a9fa282f1e24b37c
Leon Tse <leontse@hotmail.com>
2018-08-15 16:18:20 -0700

    modify regular_exp_matching

--- src/main/java/regular_expression_matching/RegularExpressionMatching.java
+++ src/main/java/regular_expression_matching/RegularExpressionMatching.java
@@ -5,36 +5,23 @@ public class RegularExpressionMatching {
 
     public class Solution {
         public boolean isMatch(String s, String p) {
-            boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
-            dp[0][0] = true;
-            for (int i = 1; i <= p.length(); i++) {
-                dp[i][0] = p.charAt(i - 1) == '*' && dp[i - 2][0];
-                for (int j = 1; j <= s.length(); j++) {
-                    if (p.charAt(i - 1) == '*') {
-                        if (dp[i - 2][j]) {
-                            dp[i][j] = true;
-                            continue;
-                        }
-                        char prev = p.charAt(i - 2);
-                        for (int k = j; k > 0; k--) {
-                            if (prev != '.' && prev != s.charAt(k - 1)) {
-                                break;
-                            }
-                            if (dp[i - 2][k - 1]) {
-                                dp[i][j] = true;
-                                break;
-                            }
-                        }
-                    } else {
-                        dp[i][j] = dp[i - 1][j - 1]
-                                && (p.charAt(i - 1) == '.' || p.charAt(i - 1) == s
-                                        .charAt(j - 1));
-                    }
-                }
-
+            if (p.isEmpty()) return s.isEmpty();
+            if (p.length() == 1) {
+                return (s.length() == 1 && (s.charAt(0) == p.charAt(0) ||
+                        p.charAt(0) == '.'));
             }
-            return dp[p.length()][s.length()];
+            if (p.charAt(1) != '*') {
+                if (s.isEmpty()) return false;
+                return (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.') &&
+                        isMatch(s.substring(1), p.substring(1));
+            }
+            while (!s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.')) {
+                if (isMatch(s, p.substring(2))) return true;
+                s = s.substring(1);
+            }
+            return isMatch(s, p.substring(2));
         }
+
     }
 
     public static class UnitTest {
