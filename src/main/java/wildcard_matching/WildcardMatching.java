c17cba92dff80b3059611b41cf7e2fd9973e21bd
Leon Tse <leontse@hotmail.com>
2018-08-15 19:09:43 -0700

    modify wildcard_matching

--- src/main/java/wildcard_matching/WildcardMatching.java
+++ src/main/java/wildcard_matching/WildcardMatching.java
@@ -4,41 +4,25 @@ public class WildcardMatching {
 
     public class Solution {
 
-        private int minLength(String p) {
-            int len = 0;
-            for (int i = 0; i < p.length(); i++) {
-                if (p.charAt(i) != '*') {
-                    len++;
-                }
-            }
-            return len;
-        }
-
         public boolean isMatch(String s, String p) {
-            if (s.length() < minLength(p)) {
-                return false;
+            int m = s.length(), n = p.length();
+            boolean[][] dp = new boolean[m+1][n+1];
+            dp[0][0] = true;
+            for (int i = 1; i <= n; ++i) {
+                if (p.charAt(i-1) == '*') dp[0][i] = dp[0][i - 1];
             }
-            boolean[] dp = new boolean[s.length() + 1];
-            dp[0] = true;
-            for (int i = 1; i <= p.length(); i++) {
-                for (int j = s.length(); j > 0; j--) {
-                    if (p.charAt(i - 1) == '*') {
-                        for (int k = 0; k <= j; k++) {
-                            if (dp[k]) {
-                                dp[j] = true;
-                                break;
-                            }
-                        }
+            for (int i = 1; i <= m; ++i) {
+                for (int j = 1; j <= n; ++j) {
+                    if (p.charAt(j-1) == '*') {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
                     } else {
-                        dp[j] = dp[j - 1]
-                                && (p.charAt(i - 1) == '?' || p.charAt(i - 1) == s
-                                        .charAt(j - 1));
+                        dp[i][j] = (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?') && dp[i - 1][j - 1];
                     }
                 }
-                dp[0] = p.charAt(i - 1) == '*' && dp[0];
             }
-            return dp[s.length()];
+            return dp[m][n];
         }
+
     }
 
     public static class UnitTest {
