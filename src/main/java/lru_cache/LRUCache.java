0ab822895d6ecf4705b540cd0be28d7fc408893f
Leon Tse <leontse@hotmail.com>
2018-08-16 20:54:23 -0700

    add lru_cache

--- src/main/java/lru_cache/LRUCache.java
+++ src/main/java/lru_cache/LRUCache.java
@@ -1,30 +1,71 @@
 package lru_cache;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.HashMap;
 
 public class LRUCache {
-    private LinkedHashMap<Integer, Integer> map;
+    private class Node{
+        Node prev;
+        Node next;
+        int key;
+        int value;
 
-    @SuppressWarnings("serial")
-    public LRUCache(final int capacity) {
-        assert capacity > 0;
-        map = new LinkedHashMap<Integer, Integer>(16, 0.75f, true) {
-            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-                return size() > capacity;
-            }
-        };
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    private int capacity;
+    private HashMap<Integer, Node> hs = new HashMap<Integer, Node>();
+    private Node head = new Node(-1, -1);
+    private Node tail = new Node(-1, -1);
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        tail.prev = head;
+        head.next = tail;
     }
 
     public int get(int key) {
-        Integer value = map.get(key);
-        if (value == null) {
+        if( !hs.containsKey(key)) {
             return -1;
         }
-        return value;
+
+        // remove current
+        Node current = hs.get(key);
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+
+        // move current to tail
+        move_to_tail(current);
+
+        return hs.get(key).value;
     }
 
     public void put(int key, int value) {
-        map.put(key, value);
+        // get 这个方法会把key挪到最末端，因此，不需要再调用 move_to_tail
+        if (get(key) != -1) {
+            hs.get(key).value = value;
+            return;
+        }
+
+        if (hs.size() == capacity) {
+            hs.remove(head.next.key);
+            head.next = head.next.next;
+            head.next.prev = head;
+        }
+
+        Node insert = new Node(key, value);
+        hs.put(key, insert);
+        move_to_tail(insert);
+    }
+
+    private void move_to_tail(Node current) {
+        current.prev = tail.prev;
+        tail.prev = current;
+        current.prev.next = current;
+        current.next = tail;
     }
 }
