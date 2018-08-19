9512726e3dde205791b95fe465694a4e89a6486c
Leon Tse <leontse@hotmail.com>
2018-08-19 15:32:20 -0700

    add valid_word_square

---
+++ src/main/java/valid_word_square/ValidWordSquare.java
@@ -0,0 +1,34 @@
+package valid_word_square;
+
+/**
+ * Created by lxie on 8/19/18.
+ */
+public class ValidWordSquare {
+
+    public class Solution {
+        public boolean validWordSquare(String[] words) {
+            // Write your code here
+            char[][] square = new char[words.length][words.length];
+
+            for (int i = 0; i < words.length; i++) {
+                square[i] = words[i].toCharArray();
+            }
+
+            for (int i = 0; i < square.length; i++) {
+                for (int j = 0; j <= i; j++) {
+                    if (square[i][j] != square[j][i]) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+        
+    }
+
+    public class UnitTest {
+
+
+    }
+}
