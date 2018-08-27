5a2a741af4788faeed21b7dc976070ad92cf928b
Leon Tse <leontse@hotmail.com>
2018-08-27 01:00:24 -0700

    modify longest_consecutive_sequence

--- src/main/java/longest_consecutive_sequence/LongestConsecutiveSequence.java
+++ src/main/java/longest_consecutive_sequence/LongestConsecutiveSequence.java
@@ -1,30 +1,26 @@
 package longest_consecutive_sequence;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LongestConsecutiveSequence {
 
     public class Solution {
-        public int longestConsecutive(int[] num) {
-            HashSet<Integer> nums = new HashSet<Integer>();
-            for (int n : num) {
-                nums.add(n);
-            }
-            int ans = 0;
-            for (int n : num) {
-                int i = n - 1;
-                while (nums.contains(i)) {
-                    nums.remove(i);
-                    i--;
+        public int longestConsecutive(int[] nums) {
+            int res = 0;
+            Map<Integer, Integer> m = new HashMap<>();
+            for (int d : nums) {
+                if (!m.containsKey(d)) {
+                    int left = m.containsKey(d - 1) ? m.get(d - 1) : 0;
+                    int right = m.containsKey(d + 1) ? m.get(d + 1) : 0;
+                    int sum = left + right + 1;
+                    m.put(d, sum);
+                    res = Math.max(res, sum);
+                    m.put(d - left, sum);
+                    m.put(d + right, sum);
                 }
-                int j = n + 1;
-                while (nums.contains(j)) {
-                    nums.remove(j);
-                    j++;
-                }
-                ans = Math.max(ans, j - i - 1);
             }
-            return ans;
+            return res;
         }
     }
 
