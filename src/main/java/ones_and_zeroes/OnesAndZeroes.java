d1b76821dd0074ef1af88012ed7df6461ef419ea
Leon Tse <leontse@hotmail.com>
2018-09-10 21:03:19 -0700

    add ones_and_zeroes

---
+++ src/main/java/ones_and_zeroes/OnesAndZeroes.java
@@ -0,0 +1,37 @@
+package ones_and_zeroes;
+
+/**
+ * Created by lxie on 9/10/18.
+ */
+public class OnesAndZeroes {
+
+    public class Solution {
+
+        public int findMaxForm(String[] strs, int m, int n) {
+            int[][] dp = new int[m+1][n+1];
+            for (String str : strs) {
+                int zeros = 0, ones = 0;
+                for (char c : str.toCharArray()) {
+                    if (c == '0') ++zeros;
+                    else
+                        ++ones;
+                }
+                for (int i = m; i >= zeros; --i) {
+                    for (int j = n; j >= ones; --j) {
+                        dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1);
+                    }
+                }
+            }
+            return dp[m][n];
+        }
+
+
+    }
+
+    public class UnitTest {
+
+
+    }
+
+
+}
