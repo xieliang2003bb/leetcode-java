41b3c4e8a37e4930347428a690d2acd65403ea37
Leon Tse <leontse@hotmail.com>
2018-09-30 22:21:42 -0700

    add palindromic_substring

--- src/main/java/palindromic_substring/PalindromicSubstring.java
+++ src/main/java/palindromic_substring/PalindromicSubstring.java
@@ -12,7 +12,7 @@ public class PalindromicSubstring {
             boolean[][] dp = new boolean[n][n];
             for (int i = n - 1; i >= 0; --i) {
                 for (int j = i; j < n; ++j) {
-                    dp[i][j] = (s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1]);
+                    dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1]);
                     if (dp[i][j]) ++res;
                 }
             }
@@ -21,7 +21,7 @@ public class PalindromicSubstring {
     }
 
     public class UnitTest {
-        
+
     }
 
 }
