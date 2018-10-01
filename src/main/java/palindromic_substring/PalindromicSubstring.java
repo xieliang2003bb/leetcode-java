212ea9316004d0cda4a5feb31e2c68ecb9da0326
Leon Tse <leontse@hotmail.com>
2018-09-30 22:18:09 -0700

    add palindromic_substring

---
+++ src/main/java/palindromic_substring/PalindromicSubstring.java
@@ -0,0 +1,27 @@
+package palindromic_substring;
+
+/**
+ * Created by lxie on 9/30/18.
+ */
+public class PalindromicSubstring {
+
+    public class Solution {
+
+        public int countSubstrings(String s) {
+            int n = s.length(), res = 0;
+            boolean[][] dp = new boolean[n][n];
+            for (int i = n - 1; i >= 0; --i) {
+                for (int j = i; j < n; ++j) {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1]);
+                    if (dp[i][j]) ++res;
+                }
+            }
+            return res;
+        }
+    }
+
+    public class UnitTest {
+        
+    }
+
+}
